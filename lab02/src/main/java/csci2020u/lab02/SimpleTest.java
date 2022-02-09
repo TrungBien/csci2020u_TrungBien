0c92dafac9cae6bd5c5ee1403d18682b3b705317
TrungBien <trung.bien@ontariotechu.net>
2022-02-09 18:48:09 -0500

    Added apache commons dependancy and CSVDemo.java

--- lab02/src/main/java/csci2020u/lab02/SimpleTest.java
+++ lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -3,7 +3,7 @@ package csci2020u.lab02;
 public class SimpleTest{
     public static void main(String[] args) {
 
-        System.out.println("pain");
+        System.out.println("Check");
     }
 }
 
